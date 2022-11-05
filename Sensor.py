2c6fa4f009aa525ff22fa1987e27f9c1e40b1afb
Eric Raslich <rasliche@gmail.com>
2022-11-04 21:46:33 -0400

    IMplement Sensor SubClasses

--- Sensor.py
+++ Sensor.py
@@ -6,3 +6,15 @@ class Sensor(Component):
 
     def detect():
         print("Detecting...")
+
+class TouchSensor(Sensor):
+    def __init__(self, status):
+        super().__init__(status)
+
+class InfraredSensor(Sensor):
+    def __init__(self, status):
+        super().__init__(status)
+
+class CameraSensor(Sensor):
+    def __init__(self, status):
+        super().__init__(status)
