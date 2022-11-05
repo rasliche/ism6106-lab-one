92287db9dc063fd5fa93de1685bafc300cf34eba
Eric Raslich <rasliche@gmail.com>
2022-11-05 10:25:27 -0400

    Implement increaseHeight method with max height

--- LiftingScrew.py
+++ LiftingScrew.py
@@ -2,12 +2,16 @@ from Component import Component
 
 class LiftingScrew(Component):
     MAX_HEIGHT = 5
-    
+
     def __init__(self, status, height=0):
         super().__init__(status)
         self._height = height
-        
+
     def increaseHeight(self, value):
+        if self._height + value >= LiftingScrew.MAX_HEIGHT:
+            self._height = LiftingScrew.MAX_HEIGHT
+        else:
+            self._height += value
         print("Raising lifting screw...")
 
     def decreaseHeight(self, value):
