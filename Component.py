36fbb2d7bba7aaa6d5c7aa03b95796d55c17aa5a
Eric Raslich <rasliche@gmail.com>
2022-11-06 21:22:10 -0500

    Update documentation of Component class

--- Component.py
+++ Component.py
@@ -1,13 +1,22 @@
 """
-The base Component Class
+The base Component class
 """
 class Component():
-    def __init__ (self, status):
+    """
+    Initialize with a status of 'ok' by default
+    """
+    def __init__ (self, status="ok"):
         self._status = status
 
+    """
+    Run diagnostics for component.
+    """
     def diagnose(self):
         print(self._status)
 
+    """
+    Update status of Component
+    """
     def setStatus(self, status):
         self._status = status
         print("Set status to {status}.")
--- RobotApp.py
+++ RobotApp.py
@@ -1,4 +1,7 @@
 from ControlUnit import ControlUnit
 
+# Demonstration of the capabilities of the robot
+# Prints output to the terminal
+
 if __name__ == '__main__':
     pass
